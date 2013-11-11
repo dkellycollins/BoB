c55756182b921205bd509dd71ce99e34c22cda65
Devin Kelly-Collins <dkellycollins@gmail.com>
2013-11-10 21:29:10 -0500

    Starting ruby project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cis560Teamprojects::Application.config.secret_token = 'fc1ada6ab6037073e1ac579d9c19a80499ea9e4f44542268777999490553c45f155827068c8554d1c47b8ef991228a4ad5f88276cecc8748ca6a6eb510457194'
