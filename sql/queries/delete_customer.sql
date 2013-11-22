ab70af7f605cba2c7a21649121928a6aeec0ceb0
mahart <mahart@users.noreply.github.com>
2013-11-22 14:56:00 -0600

    Update delete_customer.sql
    Fixed a stupid typo

--- sql/queries/delete_customer.sql
+++ sql/queries/delete_customer.sql
@@ -1,7 +1,7 @@
 /*This query will delete a customer with given C_ID*/
-DELETE FROM `Customer_Bank`
+DELETE FROM `Customer`
 WHERE C_ID =1;
 
 /*This query will delete all the customers with a given first and last name*/
-DELETE FROM `Customer_Bank`
+DELETE FROM `Customer`
 WHERE First_Name = 'Testy' AND Last_Name = 'Tester';
