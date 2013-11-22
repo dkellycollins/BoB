4aaf314d01b40af84a222a0e6ac865291f8ba681
mahart <mahart@users.noreply.github.com>
2013-11-22 15:13:31 -0600

    Update customer_value.sql
    fixed a type

--- sql/queries/customer_value.sql
+++ sql/queries/customer_value.sql
@@ -5,6 +5,6 @@ WHERE a.C_ID =1;
 
 /* Query to total a customer's value give the customer's First and last name*/
 SELECT SUM(a.Balance) 
-FROM Customer_Bank c, Account a
+FROM Customer c, Account a
 WHERE c.First_name = 'Herp' AND c.Last_Name='Derp' AND
     a.C_ID = c.C_ID;
