1c5a295e5190e9747110c6675eed4b8894f70d30
Devin Kelly-Collins <dkellycollins@gmail.com>
2013-11-22 03:03:57 -0500

    Fixed up triggers

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,29 +1,33 @@
-DELIMETER $
+DROP TRIGGER IF EXISTS transfer_trig;
+DELIMITER $
 CREATE TRIGGER transfer_trig AFTER INSERT ON Transfer
 FOR EACH ROW
 BEGIN
-  IF EXISTS (SELECT * FROM Bitcoin where A_ID = New.Buyer_A_id) THEN
-    UPDATE Bitcoin SET Balance = (New.Balance + Bitcoin.Balance) WHERE Bitcoin.A_ID = New.Buyer_ID;
-    UPDATE Bitcoin SET Balance = (Bitcoin.Balance - New.Balance) WHERE Bitcoin.A_ID = New.Seller_ID;
+  IF EXISTS (SELECT * FROM Bitcoin where A_ID = New.Buyer_A_ID) THEN
+    UPDATE Bitcoin SET Balance = (NEW.Cash_transfer + Bitcoin.Balance) 
+        WHERE Bitcoin.A_ID = NEW.Buyer_A_ID;
+    UPDATE Bitcoin SET Balance = (Bitcoin.Balance - NEW.Cash_transfer) 
+        WHERE Bitcoin.A_ID = NEW.Seller_A_ID;
   ELSE
-    UPDATE Account SET Balance = (New.Balance + Account.Balance) WHERE Account.A_ID = New.Buyer_ID;
-    UPDATE Account SET Balance = (Account.Balance - New.Balance) WHERE Account.A_ID = New.Seller_ID;
+    UPDATE Account SET Balance = (NEW.Cash_transfer + Account.Balance) 
+        WHERE Account.A_ID = NEW.Buyer_A_ID;
+    UPDATE Account SET Balance = (Account.Balance - NEW.Cash_transfer) 
+        WHERE Account.A_ID = NEW.Seller_A_ID;
   END IF;
-  END;
-$
-DELIMITER ;
+END;
     
-
-delimiter |
-CREATE TRIGGER `Upd_Balances` AFTER UPDATE ON `Bank`
+DROP TRIGGER IF EXISTS Upd_Balances;
+CREATE TRIGGER Upd_Balances AFTER UPDATE ON Bank
 FOR EACH ROW
 BEGIN
     IF NEW.Current_Bitcoin_Value <> OLD.Current_Bitcoin_Value THEN
-        UPDATE `Account` AS ac SET `Balace` = (Balance *(NEW.Current_Bitccoin_Value/OLD.Current_Bitccoin_Value))
+        UPDATE Account AS ac SET Account.Balance = (Account.Balance *(NEW.Current_Bitcoin_Value/OLD.Current_Bitcoin_Value))
         WHERE EXISTS ( SELECT *
-                        FROM `Bitcoin_Account` AS ba
+                        FROM Bitcoin_Account AS ba
                         WHERE ac.B_ID= NEW.B_ID AND ac.A_ID = ba.A_ID);
     END IF;
 END;
-|
-delimiter ;
+$
+DELIMITER ;
+
+
