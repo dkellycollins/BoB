352da4e74d225d95df68c5e2c50770ebd9248377
mahart <mahart@users.noreply.github.com>
2013-11-22 14:42:55 -0600

    Update update_customer.sql
    Fixed a stupid typo

--- sql/queries/update_customer.sql
+++ sql/queries/update_customer.sql
@@ -1,5 +1,5 @@
 /* This query will be used to change a Customer's name, given a C_ID*/
-UPDATE `Customer_Bank` SET `First_Name` = 'Testy',
+UPDATE `Customer` SET `First_Name` = 'Testy',
     `Last_Name`='Tester'
 WHERE C_ID = 1;
 
@@ -7,5 +7,5 @@ WHERE C_ID = 1;
 UPDATE `Contact_Info` AS ci SET `Phone_num` = '111-111-1111',
     `Address_1` = '123 Steet Street', `Address_2` = 'Apt 111', `Zip`=123456
 WHERE ci.CI_ID = (SELECT c.CI_ID
-                   FROM Customer_Bank AS c
+                   FROM Customer AS c
                     WHERE c.C_ID=1);
