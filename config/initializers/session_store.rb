f62be5269d61e7038a1c6c34580263d43b61e1d0
Devin Kelly-Collins <dkellycollins@gmail.com>
2013-12-07 14:55:58 -0500

    Removed ruby files

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Cis560Teamprojects::Application.config.session_store :cookie_store, key: '_cis560_teamprojects_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Cis560Teamprojects::Application.config.session_store :active_record_store
